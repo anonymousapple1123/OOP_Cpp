4d17856753f70f6c751a5f9826b8c1918a7bd1ca
Ayush Kumar <kumarayush9651@tutanota.com>
2025-05-21 15:42:29 +0500

    Compiled tested the files

---
+++ build/CMakeFiles/MyApp.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyApp.dir/link.d"
+  "CMakeFiles/MyApp.dir/main.cpp.o"
+  "CMakeFiles/MyApp.dir/main.cpp.o.d"
+  "CMakeFiles/MyApp.dir/review.cpp.o"
+  "CMakeFiles/MyApp.dir/review.cpp.o.d"
+  "MyApp"
+  "MyApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
